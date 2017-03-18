94a7f5291644e0550ab7402b60e717691baf3eb0
Shashank Koshal <shashankkoshal@hotmail.com>
2017-03-18 15:13:39 +0530

    Started new project

---
+++ Bankk.java
@@ -0,0 +1,19 @@
+Class Bank
+{
+String name;
+int age;
+int accountnumber;
+
+void get_name("String name_i")
+
+name= name_i
+
+public static void main(String args[])
+{
+Bank.bn = new Bank();
+
+bn.get_name("Shashank");
+
+system.println(get_name.bn)
+
+}}
